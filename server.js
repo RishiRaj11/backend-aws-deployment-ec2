ae12e3f89dab3d8fa2c9e5efb75a1521ea1ecd8a
Rishi Raj <rishiraj@Rishis-MacBook-Air.local>
2025-03-16 11:25:05 +0530

    coderrishirajbackend

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from "dotenv"
+import cors from "cors"
+import userRoutes from "./routes/userRoutes.js"
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error(err));
+
+// Routes
+app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
